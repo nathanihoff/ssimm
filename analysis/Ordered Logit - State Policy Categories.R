daf823608e4e8baffa663a327aea56fa11eeeee5
Kristopher Velasco <kristophervelasco@Kristophers-MacBook-Pro.local>
2020-11-19 12:31:27 -0600

    Updated models

--- analysis/Ordered Logit - State Policy Categories.R
+++ analysis/Ordered Logit - State Policy Categories.R
@@ -16,21 +16,22 @@ hist(state_policy$state_policy)
 state_policy$cat <- cut(state_policy$state_policy, breaks = c(-2,0,2, Inf), labels = c("Repressive", "Neutral", "Progressive"))    
 
 ## Getting this onto the ACS data now
-acs_dyad <- read_csv(here('data', 'acs_dyad.csv')) %>%
-  mutate(mean_year_immig = ifelse(mean_year_immig >= 1991, 
-                                  round(mean_year_immig),
+acs_coupled_imms <- read.csv("~/Documents/UT-Austin/Research Projects/Same-Sex Migration - Nathan/Datasets/acs_coupled_imms.csv")%>%
+  mutate(yrimmig = ifelse(yrimmig >= 1991, 
+                                  round(yrimmig),
                                   1991))
-acs_dyad_policy <- acs_dyad %>%
-  left_join(state_policy,by = c('mean_year_immig' = 'Year', 'state' = 'State'))
 
-acs_dyad_policy_log <- acs_dyad_policy %>%
-  mutate(same_sex_stock = log(same_sex_stock +1),
-         opp_sex_stock = log(opp_sex_stock+1),
-         state_stock_year = log(state_stock_year+1))
+lgbt_policy <- read_csv(here('data', 'LGBT Data w IPUMS Code.csv')) %>%
+  select(Country, year, Code, origin_score = total_score)
+
+acs_couple_policy <- acs_coupled_imms  %>%
+  left_join(lgbt_policy, by = c('yrimmig' = 'year', 'bpldid' = 'Code')) %>%
+  left_join(state_policy,by = c('year' = 'Year', 'state' = 'State'))
+
 
 ## fit ordered logit model and store results 'ologit'
 library(MASS)
-ologit <- polr(cat ~ same_sex_stock +  factor(year) , data = acs_dyad_policy_log, Hess=TRUE)
+ologit <- polr(cat ~ age + factor(educ) + factor(same_sex)*origin_score + factor(nchild) + yrimmig,  data = acs_couple_policy, Hess=TRUE)
 
 ## view a summary of the model
 summary(ologit)
